f077b4134f5f12569084f3c7ce3d628832d3a8d9
LuckGets <kasdies12@gmail.com>
2025-01-05 15:31:11 +0700

    Change cors domain origin

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ import { HTTPMethod } from './common/http.type';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: 'http://localhost:3000',
+      origin: ['http://localhost:3000', 'http://localhost:5173'],
       methods: [HTTPMethod.Get, HTTPMethod.Post, HTTPMethod.Patch],
       credentials: true,
     },
